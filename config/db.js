6a6fa837c3b5487533fb0cbaf2d7fa60ef7f3ddf
Alexandr Pogodin <37239276+AlexandrPogodin@users.noreply.github.com>
2019-10-13 20:49:14 +0400

    Update db settings

--- config/db.js
+++ config/db.js
@@ -1,7 +1,5 @@
 require('dotenv').config();
 
 module.exports = {
-  url: `mongodb+srv://${process.env.DB_USER}:${
-    process.env.DB_PASSWORD
-  }@cluster0-cqbcb.mongodb.net/${process.env.DB_NAME}`,
+  url: `${process.env.DB_URL}`,
 };
