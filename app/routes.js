5c4efd632059d9c9384f6bffa803ecd89ce1e67e
Alexandr Pogodin <37239276+AlexandrPogodin@users.noreply.github.com>
2019-10-13 20:51:14 +0400

    Update

--- app/routes.js
+++ app/routes.js
@@ -28,7 +28,7 @@ module.exports = function(app, passport) {
   app.post(
     '/login',
     passport.authenticate('local-login', {
-      successRedirect: '/profile', // redirect to the secure profile section
+      successRedirect: '/', // redirect to the secure main page
       failureRedirect: '/login', // redirect back to the signup page if there is an error
       failureFlash: true, // allow flash messages
     })
